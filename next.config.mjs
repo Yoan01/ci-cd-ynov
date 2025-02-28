9c0df4f9645e78f2f9c86a736d66e0ee04c15d86
Yoan Ranchon <yoan.ranchon@gmail.com>
2025-02-28 11:05:21 +0100

    fix: deploy

--- next.config.mjs
+++ next.config.mjs
@@ -6,8 +6,8 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
-  // basePath: '/ci-cd-ynov', // à retirer quand on dev en local permet d'avoir le css dans le deploiement
-  // assetPrefix: '/ci-cd-ynov/', // à retirer quand on dev en local permet d'avoir le css dans le deploiement
+  basePath: process.env.NEXT_PUBLIC_ENV === 'prod' && '/ci-cd-ynov', // à retirer quand on dev en local permet d'avoir le css dans le deploiement
+  assetPrefix: process.env.NEXT_PUBLIC_ENV === 'prod' &&  '/ci-cd-ynov/', // à retirer quand on dev en local permet d'avoir le css dans le deploiement
 };
 
 export default nextConfig;
