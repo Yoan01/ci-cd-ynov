e70335cfbe5e9b7361bca31695ea2b139faf086e
Yoan Ranchon <yoan.ranchon@gmail.com>
2025-02-28 11:41:09 +0100

    test

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  distDir: "build",
-  images: {
-    unoptimized: true,
-  },
-  basePath: process.env.NEXT_PUBLIC_ENV === 'prod' && '/ci-cd-ynov', // à retirer quand on dev en local permet d'avoir le css dans le deploiement
-  assetPrefix: process.env.NEXT_PUBLIC_ENV === 'prod' &&  '/ci-cd-ynov/', // à retirer quand on dev en local permet d'avoir le css dans le deploiement
+  // reactStrictMode: true,
+  // output: "export",
+  // distDir: "build",
+  // images: {
+  //   unoptimized: true,
+  // },
+  // basePath: process.env.NEXT_PUBLIC_ENV === 'prod' && '/ci-cd-ynov', // à retirer quand on dev en local permet d'avoir le css dans le deploiement
+  // assetPrefix: process.env.NEXT_PUBLIC_ENV === 'prod' &&  '/ci-cd-ynov/', // à retirer quand on dev en local permet d'avoir le css dans le deploiement
 };
 
 export default nextConfig;
